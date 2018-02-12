a71f2e626b110e7507434a47c618697bf45aba07
EpicPi <piyushgk1@gmail.com>
2018-02-12 17:30:02 -0500

    click login -> login page

--- app/src/main/java/com/sdcg3/sheltersearcher/Login.java
+++ app/src/main/java/com/sdcg3/sheltersearcher/Login.java
@@ -10,4 +10,5 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
 }
--- app/src/main/java/com/sdcg3/sheltersearcher/MainActivity.java
+++ app/src/main/java/com/sdcg3/sheltersearcher/MainActivity.java
@@ -1,7 +1,9 @@
 package com.sdcg3.sheltersearcher;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+    public void login(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
