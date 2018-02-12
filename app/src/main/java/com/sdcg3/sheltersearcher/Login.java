da0eb2a8a1826cd03b9bd6fce5e0aecc032416cd
EpicPi <piyushgk1@gmail.com>
2018-02-12 17:23:03 -0500

    remove bad login page, make new login page and default page

--- app/src/main/java/com/sdcg3/sheltersearcher/Login.java
+++ app/src/main/java/com/sdcg3/sheltersearcher/Login.java
@@ -3,12 +3,11 @@ package com.sdcg3.sheltersearcher;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_login);
     }
 }
